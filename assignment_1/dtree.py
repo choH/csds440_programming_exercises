80eb23ad5eea6ff1ac2a08d2e41f3e8039adc073
choH <henryzhongwork@hotmail.com>
2020-09-18 14:59:07 -0400

    ✨ Make basic Decision_Tree() class.

--- assignment_1/dtree.py
+++ assignment_1/dtree.py
@@ -7,3 +7,21 @@ def entropy(data):
     entropy = scipy.stats.entropy(data_amount)
     return entropy
 
+class Decision_Tree():
+
+    def __init__(self, depth = None):
+        self._max_depth = depth
+        self._depth = 1
+
+    def IG(self, split, H_split):
+        pass
+
+    def GR(self, split, y):
+        H_split = entropy(split)
+        IG = self.IG(split, H_split)
+        GR = IG/H_split
+
+        return GR
+
+
+
