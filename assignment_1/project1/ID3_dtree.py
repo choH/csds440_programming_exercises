c89c3750fb7270186e711f9264c4e24981ed10be
choH <henryzhongwork@hotmail.com>
2020-09-25 16:52:40 -0400

    ✨ Implement calculative methods.

--- assignment_1/project1/ID3_dtree.py
+++ assignment_1/project1/ID3_dtree.py
@@ -49,3 +49,80 @@ class ID3DecisionTree:
         for index, attr in enumerate(self.A):
             self.attr2idx[attr[0]] = index
         self.root = None
+
+
+    @staticmethod
+    def _entropy(D):
+        """Get entropy of D.
+        """
+        _, y = D
+        distribution = Counter(y)
+        total = len(y)
+
+        temp = []
+        for label in distribution:
+            freq = distribution[label]
+            p = freq / total
+            temp.append(p)
+        p = np.array(temp)
+        return -np.sum(p * np.log2(p))
+
+    @staticmethod
+    def _gain(D, idx, attr_type):
+        """Get the information gain of idx attribute.
+
+            idx (int): attribute index.
+        """
+
+        ent_D = ID3DecisionTree._entropy(D)
+
+        # calculate the entropy of each attribute value
+        X, y = D
+        optimal_point = None
+
+        if attr_type != "CONTINUOUS":
+            buf_v = dict()
+            for index, sample in enumerate(X):
+                v = sample[idx]
+                if v not in buf_v:
+                    buf_v[v] = [[], []]
+                buf_v[v][0].append(sample)
+                buf_v[v][1].append(y[index])
+
+            weighted_sum_d_v = 0.0
+            total = len(y)
+            for v in buf_v:
+                D_v = tuple(buf_v[v])
+                total_v = len(D_v[1])
+                weighted_sum_d_v += total_v / total * ID3DecisionTree._entropy(
+                    D_v)
+        else:
+            optimal_point, weighted_sum_d_v = ID3DecisionTree._get_min_entropy(
+                D, idx)
+
+        return ent_D - weighted_sum_d_v, optimal_point
+
+    @staticmethod
+    def _get_IV(D, idx, optimal_point):
+        X, y = D
+        total = len(y)
+        if optimal_point is None:
+            buf_v = Counter(map(lambda x: x[idx], X))
+        else:
+            buf_v = Counter(map(lambda x: int(x[idx] <= optimal_point), X))
+
+        temp = []
+        for v in buf_v:
+            p = buf_v[v] / total
+            temp.append(p)
+        p = np.array(temp)
+        return -np.sum(p * np.log2(p))
+
+    @staticmethod
+    def _gain_ratio(D, idx, attr_type):
+        """Instead of using information, gain ratio is utilized.
+        """
+        gain, optimal_point = ID3DecisionTree._gain(D, idx, attr_type)
+        IV = ID3DecisionTree._get_IV(D, idx, optimal_point)
+        IV += 1e-8
+        return gain / IV, optimal_point
