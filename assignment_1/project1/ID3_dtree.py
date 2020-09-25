ccaf0d96d54c5d322b718294be74b62191c9b575
choH <henryzhongwork@hotmail.com>
2020-09-25 16:55:08 -0400

    ✨ Generate tree with attribute selection.
    Co-Authored-By: null <minytie@users.noreply.github.com>

--- assignment_1/project1/ID3_dtree.py
+++ assignment_1/project1/ID3_dtree.py
@@ -50,6 +50,112 @@ class ID3DecisionTree:
             self.attr2idx[attr[0]] = index
         self.root = None
 
+    def _TreeGenerate(self, D, A, v=None, depth=0):
+        """Generate a decision tree. Note that all data vectors are Lists.
+
+        Args:
+            D (Tuple[x, y]): x is feature vectors and y is label vectors.
+            A (List[Tuple(name, type)]): arrtributes index list.
+        """
+        root = TreeNode()
+        root.set_value(v)
+
+        X, y = D
+
+        # all the samples belong to the same class
+        if len(set(y)) == 1:
+            root.set_label(y[0])
+            return root
+
+        # A is empty set or all the samples are the same on arrtributes set A or achieving the maximum depth
+        if len(A) == 0 or ID3DecisionTree._is_same(X) or (
+                self.max_depth > 0 and depth + 1 > self.max_depth):
+            most_common_class = Counter(y).most_common(1)[0][0]
+            root.set_label(most_common_class)
+            return root
+
+        # get the optimal spliting attribute
+        optimal_attribute_idx, optimal_point = self._get_optimal_attribute_idx(
+            D, A)
+
+        buf_v = dict()
+        for index, sample in enumerate(X):
+            v = sample[optimal_attribute_idx]
+
+            if A[optimal_attribute_idx][1] != "CONTINUOUS":
+                if v not in buf_v:
+                    buf_v[v] = [[], []]
+                buf_v[v][0].append(sample[:optimal_attribute_idx] +
+                                   sample[optimal_attribute_idx + 1:])
+                buf_v[v][1].append(y[index])
+            else:
+                if "pos" not in buf_v:
+                    buf_v["pos"] = [[], []]
+                    buf_v["neg"] = [[], []]
+
+                if v <= optimal_point:
+                    buf_v["neg"][0].append(sample[:optimal_attribute_idx] +
+                                           sample[optimal_attribute_idx + 1:])
+                    buf_v["neg"][1].append(y[index])
+                else:
+                    buf_v["pos"][0].append(sample[:optimal_attribute_idx] +
+                                           sample[optimal_attribute_idx + 1:])
+                    buf_v["pos"][1].append(y[index])
+
+        # set the optimal attribute for root
+        root.set_attr(A[optimal_attribute_idx][0])
+        root.set_attr_idx(self.attr2idx[A[optimal_attribute_idx][0]])
+        new_A = A[:optimal_attribute_idx] + A[optimal_attribute_idx + 1:]
+
+        for v in buf_v:
+            D_v = tuple(buf_v[v])
+            if A[optimal_attribute_idx][1] != "CONTINUOUS":
+                if D_v[0]:
+                    root.add_child(self._TreeGenerate(D_v, new_A, v,
+                                                      depth + 1))
+            else:
+                if D_v[0]:
+                    if v == "pos":
+                        root.add_child(
+                            self._TreeGenerate(D_v, new_A,
+                                               (">", optimal_point),
+                                               depth + 1))
+                    else:
+                        root.add_child(
+                            self._TreeGenerate(D_v, new_A,
+                                               ("<", optimal_point),
+                                               depth + 1))
+
+        return root
+
+    def _get_optimal_attribute_idx(self, D, A):
+        """Get the optimal attribute.
+        """
+        attributes_num = len(D[0][0])
+
+        if self.criterion == "gain":
+            criterion_func = ID3DecisionTree._gain
+        else:
+            criterion_func = ID3DecisionTree._gain_ratio
+
+        maxm_criterion = float('-inf')
+        optimal_attribute_idx = -1
+        optimal_point = None
+
+        for idx in range(attributes_num):
+            cur_criterion, cur_optimal_point = criterion_func(
+                D, idx, A[idx][1])
+            if cur_criterion > maxm_criterion:
+                maxm_criterion = cur_criterion
+                optimal_attribute_idx = idx
+                optimal_point = cur_optimal_point
+
+        return optimal_attribute_idx, optimal_point
+
+    @staticmethod
+    def _is_same(X):
+        X_ = map(lambda x: tuple(x), X)
+        return len(set(X_)) == 1
 
     @staticmethod
     def _entropy(D):
@@ -66,6 +172,56 @@ class ID3DecisionTree:
             temp.append(p)
         p = np.array(temp)
         return -np.sum(p * np.log2(p))
+
+    @staticmethod
+    def _get_min_entropy(D, idx):
+        """Get the split point with the minimum entropy.
+        """
+        X, y = D
+        total = len(y)
+
+        # get sorted value for idx attribute
+        sorted_attributes = sorted(map(lambda x: x[idx], X))
+
+        # get candidate split points set
+        candidates = set()
+
+        for index in range(len(sorted_attributes) - 1):
+            candidates.add(
+                (sorted_attributes[index] + sorted_attributes[index + 1]) / 2)
+
+        # sort samples
+        samples = []
+        for index, x in enumerate(X):
+            samples.append((x, y[index], x[idx]))
+        samples = sorted(samples, key=lambda x: x[2])
+
+        # get the optimal split point
+        optimal_point = None
+        minm_entropy = float('inf')
+        buf_v = dict()
+        buf_v["neg"] = [[], []]
+        buf_v["pos"] = [[], []]
+        for index, sample in enumerate(samples):
+            buf_v["pos"][0].append(sample[0])
+            buf_v["pos"][1].append(sample[1])
+        for index, point in enumerate(sorted(candidates)):
+            cur_entropy = 0.0
+
+            buf_v["neg"][0].append(buf_v["pos"][0][0])
+            buf_v["neg"][1].append(buf_v["pos"][1][0])
+            del buf_v["pos"][0][0]
+            del buf_v["pos"][1][0]
+
+            for v in buf_v:
+                D_v = tuple(buf_v[v])
+                total_v = len(D_v[1])
+                cur_entropy += total_v / total * ID3DecisionTree._entropy(D_v)
+
+            if cur_entropy < minm_entropy:
+                minm_entropy = cur_entropy
+                optimal_point = point
+        return optimal_point, minm_entropy
 
     @staticmethod
     def _gain(D, idx, attr_type):
