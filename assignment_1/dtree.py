21676e969a9703b6fe1ac682c88a55c9c05d83c3
choH <henryzhongwork@hotmail.com>
2020-09-25 19:00:14 -0400

    🚚 Move Programming Assignment 1 files from to secondary dir.

--- assignment_1/dtree.py
+++ assignment_1/dtree.py
@@ -1,64 +1,41 @@
-import pandas as pd
-import numpy as np
-import scipy.stats
+'''
+Author: Mingyang Tie, mxt497 Shaochen (Henry) ZHONG, sxz517
+Date: 2020-09-18 23:02:31
+LastEditTime: 2020-09-25 10:40:12
+'''
 import sys
+from ID3_dtree import ID3DecisionTree
+import random
 
-if len(sys.argv) < 4:
-    print("Not enough argument passed")
+random.seed(12345)
 
-data_dir = str(sys.argv[0])
+# parse args
+if len(sys.argv) != 5:
+    raise ValueError("Not proper command!")
+_, path, cv, max_depth, criterion = sys.argv
+
 try:
-    full_sample_flag = bool(int(sys.argv[0]))
-    maximal_depth = int(sys.argv[0])
-    full_tree_flag = True if maximal_depth == 0 else False
-    GR_flag = bool(int(sys.argv[0]))
+    cv = int(cv)
+    if cv:
+        cv = False
+    else:
+        cv = True
+
+    max_depth = int(max_depth)
+    if max_depth < 0:
+        raise ValueError("Not valid maximum depth.")
+    criterion = int(criterion)
+
+    if criterion:
+        criterion = "gain ratio"
+    else:
+        criterion = "gain"
+
 except ValueError:
-    print("Incorrect argument format, should be 0 or 1 for cross validation and >= 0 for depth.")
+    print("Invalid arguments.")
 
+# initialize a model instance
+ID3Tree = ID3DecisionTree(max_depth, path, criterion, cv)
 
-def entropy(data):
-    data_amount = data.value_counts()
-    entropy = scipy.stats.entropy(data_amount)
-    return entropy
-
-
-class ID3Decision_Tree: #Create class about ecision tree node
-	def __init__(self, parent, child, attribute, split, leaf, depth, branch):
-		self.parent = parent
-		self.child = child
-		self.attribute = attribute
-		self.split = split
-		self.leaf = leaf 
-		seld.depth = depth
-		self.branch = branch
-		
-	def insert_parent(self, parent): #parents of node
-		self.parent = parent #insert parent
-	
-	def insert_child(self, child, b):#all children in the node
-		self.child.append(child) # add child in children list
-		self.branch[b] = child #set values of branch of this child
-		
-	def insert_attribute(self, attribute):
-		self.attribute = attribute #Set attribute when node split
-		
-	def insert_split(self, split):
-		self.split = split #Set split value for attribute
-		
-	def insert_leaf(self, leaf):#Set class index of leaf node 
-		self.leaf = leaf
-		
-	def insert_depth(self, depth): #depth of the node
-		self.depth = depth
-        
-    	def IG(self, split, H_split):
-        	pass
-
-    	def GR(self, split, y):
-        	H_split = entropy(split)
-        	IG = self.IG(split, H_split)
-        	GR = IG/H_split
-		
-
-
-
+# run the model
+ID3Tree.run()
