a0371eaf9f4e38ad779135eba7557f4855cabe86
choH <henryzhongwork@hotmail.com>
2020-10-09 16:28:20 -0400

    ✨ Add preprocess.

---
+++ P2/preprocess.py
@@ -0,0 +1,36 @@
+'''
+Author: Mingyang Tie, mxt497 Shaochen (Henry) ZHONG, sxz517
+Date: 2020-10-05 10:45:23
+LastEditTime: 2020-10-09 14:22:38
+'''
+import numpy as np
+import pandas as pd
+from pandas import cut,qcut
+
+def process(X, prob, n_bin=3,cut_method='cut'):
+    """
+    process the data
+    :param X: data X
+    :param prob: problem name
+    :param n_bin: number of bins
+    :return:
+    """
+    cut_method=eval('pd.'+cut_method)
+    if prob == 'spam':
+        # Discretization
+        for i in range(len(X[0])):
+            if i != 5:
+                # print(X[:,i])
+
+                X[:, i] = cut_method(X[:, i], n_bin, retbins=True, labels=[x for x in range(n_bin)])[0].codes
+
+        X = X.astype(np.int32)
+    if prob == 'volcanoes':
+        X = X.astype(np.int32)
+        X=X[:,1:]#remove
+        for i in range(len(X[0])):
+            X[:, i] = cut_method(X[:, i], n_bin, retbins=True, labels=[x for x in range(n_bin)])[0].codes
+    if prob == 'voting':
+        X = X.astype(np.int32)
+
+    return X
